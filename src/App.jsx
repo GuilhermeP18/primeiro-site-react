39c7b13a81d47a8cb14b391239ce8da605eee58b
gui <luisguilhermep16@gmail.com>
2025-10-20 11:33:43 -0300

    ajustado componentes e criado main expandido

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,18 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Nav from "./components/nav.jsx";
 import Footer from "./components/footer.jsx";
 import Main from "./components/main.jsx";
-import blackBg from "./assets/black.jpeg";
+import BackgroundImage from "./components/BackgroundImage.jsx";
 
 function App() {
-  const [scrollY, setScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => setScrollY(window.scrollY);
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const backgroundStyle = {
-    background: `
-      linear-gradient(to bottom, rgba(0,0,0,0) 100%, #181818 99%),
-      url(${blackBg})
-    `,
-    backgroundSize: "120% auto",
-    backgroundRepeat: "no-repeat",
-  backgroundPosition: `center calc(-10vh - ${scrollY * 0.5}px)`,
-    minHeight: "100vh",
-    transition: "background-position 0.8s cubic-bezier(0.4, 0, 0.2, 1)"
-  };
-
   return (
-    <div style={backgroundStyle}>
-      <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+    <>
+      <BackgroundImage />
+      <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh", position: "relative", zIndex: 1 }}>
         <Nav />
         <Main />
         <Footer />
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,13 +1,25 @@
 
-import React from "react";
+
+import React, { useEffect, useState } from "react";
 import "./css/main.css";
+import fundo from "../assets/fundo.png"
 
 function Main() {
+    const [scrollY, setScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => setScrollY(window.scrollY);
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    // O texto se move na mesma proporção que a imagem de fundo
+
     return (
-        <main style={{ flex: 1, padding: "20px", minHeight: "100vh" }}>
-            <h1>SEJA BEM-VINDO</h1>
+        <main>
             <div className="mainContent">
                 <div className="contentGrade">
+                    <h2>Projetos em andamento</h2>]
                     
                 </div>
             </div>
