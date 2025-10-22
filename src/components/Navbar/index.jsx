a61c103b92083e06905b5b44a2c93a165fb867cf
gui <luisguilhermep16@gmail.com>
2025-10-22 14:06:30 -0300

    estilizado nav e responsivo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -8,11 +8,15 @@ function Nav() {
   return (
     <header >
       <nav className="navbar">
-        <ul>
-          <li ><Link to="/" className="noHover">PortiFolio</Link></li>
-          <li className="left-nav"><NavLink to="/about" >Sobre</NavLink></li>
-          <li className="left-nav"><NavLink to="/contact" >Contato</NavLink></li>
+        <ul className="left-nav">
+          <li className="siteLogo"><Link to="/" className="noHover">PortiFolio</Link></li>
         </ul>
+        <ul className="right-nav">
+          <li><NavLink to="/about" >Sobre</NavLink></li>
+          <li><NavLink to="/contact" >Contato</NavLink></li>
+        </ul>
+          
+        
       </nav>
     </header>
 
